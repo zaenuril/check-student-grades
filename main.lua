a3f59a0ce1cf531526f9652f2ac8f8cab254317f
zaenuril hakim <zainurilhakim10188@gmail.com>
2025-11-21 00:01:16 +0700

    feat: add new project

---
+++ main.lua
@@ -0,0 +1,20 @@
+
+io.write("masukan nama: ")
+local nama = io.read()
+io.write("masukan nilai: ")
+local nilai = io.read("*n")
+
+local function main()
+
+    if nilai >= 90 then
+    print(nama .. ", nilai kamu " .. nilai .. ", kamu jenius!")
+    elseif nilai >= 80 then
+        print(nama .. ", nilai kamu " .. nilai .. ", kamu pintar!")
+    elseif nilai >= 70 then
+        print(nama .. ", nilai kamu " .. nilai .. ", lumayan")
+    else
+        print("bodoh banget anjir")
+    end 
+end
+
+main()
